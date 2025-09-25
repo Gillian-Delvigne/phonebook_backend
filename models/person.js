6ec2dbf35185c8fd92da242c8704dbc049aaacb2
Gillian <gillian.delvigne@gmail.com>
2025-09-25 17:31:50 +0200

    auto commit

--- models/person.js
+++ models/person.js
@@ -1,42 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.DB_URI;
-mongoose.set("strictQuery", false);
+const url = process.env.DB_URI
+mongoose.set('strictQuery', false)
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log("connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message);
-    });
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "User name required"],
-        minLength: [3, "Name should be at least 3 characters"],
+  name: {
+    type: String,
+    required: [true, 'User name required'],
+    minLength: [3, 'Name should be at least 3 characters'],
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Phone number is too short (8 minimum)'],
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d+/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    number: {
-        type: String,
-        minLength: [8, "Phone number is too short (8 minimum)"],
-        validate: {
-            validator: function (v) {
-                return /\d{2,3}-\d+/.test(v);
-            },
-            message: (props) => `${props.value} is not a valid phone number!`,
-        },
-        required: [true, "User phone number required"],
-    },
-});
+    required: [true, 'User phone number required'],
+  },
+})
 
-personSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
-});
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
