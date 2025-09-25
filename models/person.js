d18d02c01c57bba3421537cacdfa77931eb05656
Gillian <gillian.delvigne@gmail.com>
2025-09-25 16:02:29 +0200

    auto commit

--- models/person.js
+++ models/person.js
@@ -14,11 +14,21 @@ mongoose
 
 const personSchema = new mongoose.Schema({
     name: {
-    type: String,
-    required: true,
-	minLength: 3
-  },
-    number: String,
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d+/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+        required: [true, "User phone number required"],
+    },
 });
 
 personSchema.set("toJSON", {
